7c679d978218c6b20501b6c08ea0dd7295eb3b42
Rick Skanron <rskanron@gmail.com>
2019-02-28 15:36:09 -0700

    Doing query string paramaters the right way in the framework.

--- UccSearch/Controllers/DebtorsController.cs
+++ UccSearch/Controllers/DebtorsController.cs
@@ -7,17 +7,11 @@ namespace UccSearch.Controllers
 {
     public class DebtorsController : Controller
     {
-        [HttpGet]
-        public IActionResult Index([FromServices] IMarketplaceService masterListService)
+        [HttpGet("{searchTerm}")]
+        public IActionResult Index([FromQuery] string searchTerm, [FromServices] IMarketplaceService masterListService)
         {                         
-            var searchTerm = HttpContext.Request.Query["searchTerm"].ToString();
-
-            if (string.IsNullOrEmpty(searchTerm)) {
-                return Json(new string[0]);
-            } else {
-                var data = masterListService.search("debtorname", searchTerm);
-                return Json(data);
-            }
+            var data = masterListService.search("debtorname", searchTerm);
+            return Json(data);
         } 
 
         public IActionResult Error()
